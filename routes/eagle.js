18eef326d86d0804499b2c488aa60d7c20e2254f
sumedh <143051578+S567645@users.noreply.github.com>
2023-11-15 14:30:04 -0600

    part 7

--- routes/eagle.js
+++ routes/eagle.js
@@ -12,5 +12,8 @@ router.get('/detail', eagle_controlers.eagle_view_one_Page);
 router.get('/create', eagle_controlers.eagle_create_Page);
 /* GET create update page */
 router.get('/update', eagle_controlers.eagle_update_Page);
+/* GET delete eagle page */
+router.get('/delete', eagle_controlers.eagle_delete_Page);
+
 
 module.exports = router;
