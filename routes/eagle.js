145abd760c2f91c753275a4bba281a7849b86c00
sumedh <143051578+S567645@users.noreply.github.com>
2023-11-29 11:16:12 -0600

    bonus

--- routes/eagle.js
+++ routes/eagle.js
@@ -15,13 +15,13 @@ router.get('/', function(req, res, next) {
   res.render('eagle', { title: 'Search results by Sumedh' });
 });
 /* GET detail eagle page */
-router.get('/detail', eagle_controlers.eagle_view_one_Page);
+router.get('/detail',secured, eagle_controlers.eagle_view_one_Page);
 /* GET create eagle page */
-router.get('/create', eagle_controlers.eagle_create_Page);
+router.get('/create',secured, eagle_controlers.eagle_create_Page);
 /* GET create update page */
 router.get('/update',secured, eagle_controlers.eagle_update_Page);
 /* GET delete eagle page */
-router.get('/delete', eagle_controlers.eagle_delete_Page);
+router.get('/delete',secured, eagle_controlers.eagle_delete_Page);
 
 
 module.exports = router;
